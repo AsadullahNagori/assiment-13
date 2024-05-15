64bc52c561e4b6de14a5a3ad32d02267a9cd849f
AsadullahNagori <164639593+AsadullahNagori@users.noreply.github.com>
2024-05-15 12:35:26 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+//
+let transpotation = ["Car", "Bike", "Aeroplane", "Bycycle"];
+console.log(`I would like to own a :${transpotation[0]} `);
+console.log(`I would like to own a :${transpotation[1]} `);
+console.log(`I would like to own a :${transpotation[2]} `);
+console.log(`I would like to own a :${transpotation[3]} `);
---
+++ main.ts
@@ -0,0 +1,8 @@
+//
+
+
+let transpotation:string[]=["Car","Bike","Aeroplane","Bycycle"];
+console.log(`I would like to own a :${transpotation[0]} `);
+console.log(`I would like to own a :${transpotation[1]} `);
+console.log(`I would like to own a :${transpotation[2]} `);
+console.log(`I would like to own a :${transpotation[3]} `);
